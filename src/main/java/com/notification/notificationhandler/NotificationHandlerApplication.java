0be444160cf665582c4bf1eb8c28bc1cc6e34335
mimo_ <magda_poliwka@op.pl>
2024-12-19 20:59:00 +0000

    Resolve dependencies

--- src/main/java/com/notification/notificationhandler/NotificationHandlerApplication.java
+++ src/main/java/com/notification/notificationhandler/NotificationHandlerApplication.java
@@ -1,4 +1,4 @@
-package com.example.notificationhandler;
+package com.notification.notificationhandler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
