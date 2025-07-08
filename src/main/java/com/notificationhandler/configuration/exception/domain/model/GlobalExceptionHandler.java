de9c11123950094a901e8f184a1eca462b28732e
mimo_ <magda_poliwka@op.pl>
2025-07-08 21:59:16 +0100

    Add GlobalExceptionHandler

---
+++ src/main/java/com/notificationhandler/configuration/exception/domain/model/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.notificationhandler.configuration.exception.domain.model;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handle(ResourceNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BadApiRequestException.class)
+    public ResponseEntity<String> handle(BadApiRequestException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handle(Exception exception) {
+        return new ResponseEntity<>("Internal server error", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
